6a603e84e191d16accdd7b6c3ebd6ba72a8c95a6
yh72167 <yh.19972372@gmail.com>
2022-03-21 12:29:13 +0900

    apiの開通設定と確認画面

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,22 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import useSWR from 'swr'
+import axios from '@/libs/axios'
 
-const Home: NextPage = () => {
-  return (
-    <div>Home</div>
-  )
+const Hello: NextPage = () => {
+    const { data, error } = useSWR('/api/hello', () =>
+    axios
+        .get('/api/hello')
+        .then((res: any) => res.data)
+    )
+
+    if (error) return <div>エラーが発生しました</div>
+    if (!data) return <div>読み込み中</div>
+    return (
+    <div>
+        <h1>ようこそ</h1>
+        <p>{data}</p>
+    </div>
+    )
 }
 
-export default Home
+export default Hello

61c3621db3a56727098bf56b28c8152fa8333be4
yh72167 <yh.19972372@gmail.com>
2022-03-21 12:05:14 +0900

    最初のコミット

---
+++ api/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/hello', function () {
+    return 'Hello Next.js';
+});
---
+++ client/pages/auth/login.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Login: NextPage = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
